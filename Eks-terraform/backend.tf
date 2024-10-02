fa9d0c74dd10fde66defd30126d49fbf22893f75
Datnv-Netw <datcyber2003@gmail.com>
2024-10-02 16:51:04 +0700

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "datcyber-bucket" # Replace with your actual S3 bucket name
+    key    = "datcyber/terraform.tfstate"
+    region = "us-east-1"
   }
 }
